b0257cd6c0e8ee53084ce42d2dea928c62cb3054
John Elliott <johnelliott703@gmail.com>
2017-01-12 23:17:30 -0500

    try dotenv

---
+++ es6-transpiled.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _dotenv2.default)();
+console.log(process.env.PANTS);
